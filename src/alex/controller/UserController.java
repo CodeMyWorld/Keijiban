cff8a6914493bf9cb03d280ecb9256f54bb6d6ff
Alex Lai <alexlai@softlab.cs.tsukuba.ac.jp>
2016-03-15 13:00:53 +0900

    import SpringMVC

---
+++ src/alex/controller/UserController.java
@@ -0,0 +1,53 @@
+package alex.controller;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import alex.model.User;
+import alex.service.IUserService;
+
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	@Resource(name = "userService")
+	private IUserService userService;
+
+	@RequestMapping(value="/register", method=RequestMethod.GET)
+	public ModelAndView getRegister(){
+		ModelAndView mView = new ModelAndView();
+        User user = userService.findOne(1);
+        mView.addObject("user", user);
+		mView.setViewName("user/detail");
+		return mView;
+	}
+	
+	@RequestMapping(value="/register", method=RequestMethod.POST)
+	public String add(@ModelAttribute("user") User user){
+		userService.create(user);
+		return "redirect:/user/register";
+	}
+
+	@RequestMapping(value="/show/{id}", method=RequestMethod.GET)
+	public ModelAndView showDetail(@PathVariable Integer id){
+		User user = userService.findOne(id);
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("user/detail");
+		mv.addObject("user", user);
+		System.out.println("bug");
+		return mv;
+	}
+
+	@RequestMapping(value="/test", method=RequestMethod.GET)
+	public ModelAndView getHello(){
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("user/hello");
+		return mv;
+	}
+}
---
+++ src/alex/model/User.java
@@ -0,0 +1,67 @@
+package alex.model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+@Entity(name = "User")
+@Table(name = "user")
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	public User(){
+		super();
+	}
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name="id")
+	private Integer id;
+	
+	@Column(name="username", length=45)
+	private String username;
+	
+	@Column(name="password", length=45)
+	private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "groupid")
+    private Group group;
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "username: " + this.username;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    }
+}
